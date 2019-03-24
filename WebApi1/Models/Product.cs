a633812054adf8ee6026ed341ce7a5a99388c0f9
furkan.yeke <furkan.yeke@BK-LAB502-PC3>
2019-03-24 18:45:22 +0300

    Add project files.

---
+++ WebApi1/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿ü,using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi1.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public double Weight { get; set; }
+
+    }
+}
